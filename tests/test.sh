c8e42d77207e4c3a81cfbacb403c6502246896c8
lzxm160 <lzxm160@sina.com>
2020-06-22 18:20:36 +0800

    x

--- tests/test.sh
+++ tests/test.sh
@@ -4,11 +4,13 @@ GRN=$'\e[32;1m'
 OFF=$'\e[0m'
 GW="./iotex-core-rosetta-gateway"
 printf "${GRN}### Starting the Rosetta gateway...${OFF}\n"
+cd ..
 ${GW} &
 
 sleep 3
 
 printf "${GRN}### Validating Rosetta gateway implementation...${OFF}\n"
+cd tests
 ./rosetta-cli check --lookup-balance-by-block false --end 10 --bootstrap-balances ./bootstrap_balances.json --block-concurrency 1
 ./rosetta-cli view:account '{"address":"io10t7juxazfteqzjsd6qjk7tkgmngj2tm7n4fvrd"}'
 ./rosetta-cli view:block 4034780
