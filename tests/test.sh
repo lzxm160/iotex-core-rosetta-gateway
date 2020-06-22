ab49051096876c8bb2e78b191a38e09ffba5fad8
lzxm160 <lzxm160@sina.com>
2020-06-22 17:30:22 +0800

    x

--- tests/test.sh
+++ tests/test.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-./rosetta-cli check --lookup-balance-by-block false --start 4032646 --end 4034781 --bootstrap-balances ./bootstrap_balances.json
+./rosetta-cli check --lookup-balance-by-block false --start 4032646 --end 4034781 --bootstrap-balances ./bootstrap_balances.json --block-concurrency 1
+
+./rosetta-cli check --lookup-balance-by-block false --end 10 --bootstrap-balances ./bootstrap_balances.json --block-concurrency 1
