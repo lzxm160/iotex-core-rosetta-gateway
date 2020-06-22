7893b78a63805fd75cc907e0828dc20b08e4bcc3
lzxm160 <lzxm160@sina.com>
2020-06-22 17:55:53 +0800

    x

--- tests/test.sh
+++ tests/test.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-./rosetta-cli check --lookup-balance-by-block false --start 4032646 --end 4034781 --bootstrap-balances ./bootstrap_balances.json --block-concurrency 1
-
-./rosetta-cli check --lookup-balance-by-block false --end 10 --bootstrap-balances ./bootstrap_balances.json --block-concurrency 1
+./rosetta-cli check --lookup-balance-by-block false --end 10 --bootstrap-balances ./bootstrap_balances.json
 
 ./rosetta-cli view:account '{"address":"io10t7juxazfteqzjsd6qjk7tkgmngj2tm7n4fvrd"}'
 
