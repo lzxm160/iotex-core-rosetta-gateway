d72bdb8cd062fe20fa205494b91cb20a50dc9664
lzxm160 <lzxm160@sina.com>
2020-06-22 18:07:36 +0800

    x

--- tests/test.sh
+++ tests/test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 ./rosetta-cli check --lookup-balance-by-block false --end 10 --bootstrap-balances ./bootstrap_balances.json --block-concurrency 1
-
 ./rosetta-cli view:account '{"address":"io10t7juxazfteqzjsd6qjk7tkgmngj2tm7n4fvrd"}'
-
 ./rosetta-cli view:block 4034780
+echo "clean up"
+rm -fr rosetta*
