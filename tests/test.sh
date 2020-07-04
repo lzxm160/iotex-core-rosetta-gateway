4945c518b5e0888eb181b4be6bf77f2ac9a69485
lzxm160 <lzxm160@sina.com>
2020-07-04 12:54:39 +0800

    add more action tests

--- tests/test.sh
+++ tests/test.sh
@@ -22,6 +22,8 @@ printf "${GRN}### Inject some actions...${OFF}\n"
 cd inject
 go test
 
+sleep 3 #wait for the last candidate action
+
 cd ..
 GW="./iotex-core-rosetta-gateway"
 printf "${GRN}### Starting the Rosetta gateway...${OFF}\n"
@@ -37,7 +39,8 @@ cd tests
 ./rosetta-cli create:configuration config.json
 
 printf "${GRN}### Validating Rosetta gateway implementation...${OFF}\n"
-./rosetta-cli check --lookup-balance-by-block=false --end 340 --bootstrap-balances ./bootstrap_balances.json --block-concurrency 8
+./rosetta-cli check --lookup-balance-by-block=false --end 111 --bootstrap-balances ./bootstrap_balances.json
+--block-concurrency 8
 ./rosetta-cli view:account '{"address":"io1ph0u2psnd7muq5xv9623rmxdsxc4uapxhzpg02"}'
 ./rosetta-cli view:block 10
 rm -rf /tmp/rosetta-cli*
