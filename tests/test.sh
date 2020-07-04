04e11bb080f25f34934e28664167d43341db7c37
lzxm160 <lzxm160@sina.com>
2020-07-04 19:04:38 +0800

    x

--- tests/test.sh
+++ tests/test.sh
@@ -20,10 +20,10 @@ iotex_core_tar='wget --quiet --show-progress --progress=bar:force:noscroll -O ht
 
 #download
 printf "${GRN}### Downloading rosetta-cli release %s...${OFF}\n" ${ROSETTA_CLI_RELEASE}
-	${rosetta_cli_tar}
+	wget --quiet --show-progress --progress=bar:force:noscroll -O https://github.com/coinbase/rosetta-cli/archive/v${ROSETTA_CLI_RELEASE}.tar.gz
 
 printf "${GRN}### Downloading iotex-core release ${IOTEX_SERVER_RELEASE}...${OFF}\n"
-	${iotex_core_tar}
+	wget --quiet --show-progress --progress=bar:force:noscroll -O https://github.com/iotexproject/iotex-core/archive/${IOTEX_SERVER_RELEASE}.tar.gz
 
 #build
 printf "${GRN}### Building rosetta-cli...$(OFF)\n"
