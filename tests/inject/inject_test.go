7018639f55456cd47bde57a7e21526b1e67113f9
lzxm160 <lzxm160@sina.com>
2020-07-08 17:21:19 +0800

    x

--- tests/inject/inject_test.go
+++ tests/inject/inject_test.go
@@ -240,9 +240,9 @@ func TestGetImplicitLog(t *testing.T) {
 			for _, t := range trans.GetTransactions() {
 				switch {
 				case bytes.Compare(t.GetTopic(), InContractTransfer[:]) == 0:
-					fmt.Println(i, "execution", t.Recipient)
+					fmt.Println(i, "execution", t.Sender, t.Recipient, t.Amount)
 				case bytes.Compare(t.GetTopic(), BucketWithdrawAmount[:]) == 0:
-					fmt.Println(i, "execution", t.Recipient)
+					fmt.Println(i, "stakewithdraw", t.Sender, t.Recipient, t.Amount)
 				default:
 					fmt.Println(i, "other")
 				}
