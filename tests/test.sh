22e5fb0fbd76e74dd38f2ebe74aedeb81d5c2ad1
lzxm160 <lzxm160@sina.com>
2020-07-09 14:46:20 +0800

    fix index

--- tests/test.sh
+++ tests/test.sh
@@ -63,7 +63,9 @@ cd tests
 ./rosetta-cli create:configuration config.json
 
 printf "${GRN}### Validating Rosetta gateway implementation...${OFF}\n"
-./rosetta-cli check --lookup-balance-by-block=false --end 111 --bootstrap-balances ./bootstrap_balances.json --block-concurrency 8
+#./rosetta-cli check --lookup-balance-by-block=false --end 111 --bootstrap-balances ./bootstrap_balances.json --block-concurrency 8
+./rosetta-cli check --lookup-balance-by-block=false log-balance-changes=true log-reconciliations=true --bootstrap-balances ./bootstrap_balances_mainnet.json
+
 ./rosetta-cli view:account '{"address":"io1ph0u2psnd7muq5xv9623rmxdsxc4uapxhzpg02"}'
 ./rosetta-cli view:block 10
 rm -rf /tmp/rosetta-cli*
