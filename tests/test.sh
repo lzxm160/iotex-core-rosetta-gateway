b10681e08f4cb9c20d2424d641138aea110126c1
lzxm160 <lzxm160@sina.com>
2020-07-10 12:22:25 +0800

    x

--- tests/test.sh
+++ tests/test.sh
@@ -66,6 +66,7 @@ printf "${GRN}### Validating Rosetta gateway implementation...${OFF}\n"
 ./rosetta-cli check --lookup-balance-by-block=false --end 111 --bootstrap-balances ./bootstrap_balances.json --exempt-accounts ./exempt_accounts.json --data-dir=./ --log-balance-changes=true --log-reconciliations=true --block-concurrency 8
 ./rosetta-cli view:account '{"address":"io1ph0u2psnd7muq5xv9623rmxdsxc4uapxhzpg02"}'
 ./rosetta-cli view:block 10
+sleep 1000000000000
 rm -rf /tmp/rosetta-cli*
 
 # Clean up after a successful run.
