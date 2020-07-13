86baa0df655f3c039d8f0dbcc32e1519c09e0352
lzxm160 <lzxm160@sina.com>
2020-07-13 11:11:51 +0800

    x

--- iotex-client/util.go
+++ iotex-client/util.go
@@ -71,6 +71,7 @@ func getActionType(topic []byte) string {
 	BucketCreateAmount := hash.BytesToHash256([]byte("createAmount"))
 	BucketDepositAmount := hash.BytesToHash256([]byte("depositAmount"))
 	CandidateRegistrationFee := hash.BytesToHash256([]byte("registrationFee"))
+	CandidateSelfStake := hash.BytesToHash256([]byte("selfStake"))
 	switch {
 	case bytes.Compare(topic, InContractTransfer[:]) == 0:
 		return Execution
@@ -81,6 +82,8 @@ func getActionType(topic []byte) string {
 	case bytes.Compare(topic, BucketDepositAmount[:]) == 0:
 		return StakeAddDeposit
 	case bytes.Compare(topic, CandidateRegistrationFee[:]) == 0:
+		return CandidateRegister
+	case bytes.Compare(topic, CandidateSelfStake[:]) == 0:
 		return CandidateRegister
 	}
 	return ""
